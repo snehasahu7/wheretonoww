1ee2a24880d08dc4371e85957b0caec88c986caa
snehasahu7 <btech15146.22@bitmesra.ac.in>
2025-06-05 19:29:31 +0530

    Updated

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import Navbar from './Components/Navbar/Navbar.jsx'
-import Footer from './Components/footer/Footer.jsx'
+import Footer from './Components/Footer/Footer.jsx'
 import Hero from './Components/Hero/Hero.jsx'
 
 
