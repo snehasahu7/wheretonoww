7321f42bebc9c5ac8c7de91e38fc7638aa4f512f
snehasahu7 <btech15146.22@bitmesra.ac.in>
2025-06-05 19:39:23 +0530

    Updated backend

--- Backend/index.js
+++ Backend/index.js
@@ -16,7 +16,7 @@ const app = express();
 const port = 4000;
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://wheretonoww.netlify.app',
     credentials: true,
     methods: ['GET', 'POST'],
     allowedHeaders: ['Content-Type']
